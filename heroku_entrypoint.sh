6aeb8b51457c5aefda194e1431946898a7d540b1
Tomoya Ishii <shi2gx@gmail.com>
2021-04-10 02:29:01 +0900

    [Fix] Heroku entrypoint

--- heroku_entrypoint.sh
+++ heroku_entrypoint.sh
@@ -2,10 +2,10 @@
 
 psql -c "CREATE DATABASE minimaid;" "$DATABASE_URL"
 
-DATABASE_URL=$(sed -e 's_postgres://_postgresql+asyncpg://_' "$DATABASE_URL")
-# shellcheck disable=SC2016
-DATABASE_URL=$(sed -e 's_$_/minimaid_' "$DATABASE_URL")
+DATABASE_URL="${DATABASE_URL//postgres:\/\//postgresql+asyncpg:\/\/}"
 
 export DATABASE_URL="$DATABASE_URL"
+
+echo "$DATABASE_URL"
 
 python main.py
