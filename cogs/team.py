3f2935436e18291320571e42a792d2409acd887a
Sumito Izumita <sumito@izumita.com>
2021-04-11 19:28:15 +0900

    [Add] chunked teaming command

--- cogs/team.py
+++ cogs/team.py
@@ -1,5 +1,6 @@
-from discord.ext.commands import Cog, Context, group
-from typing import TYPE_CHECKING, List
+from discord.ext.commands import Cog, Context, group, guild_only
+from typing import TYPE_CHECKING
+from more_itertools import chunked
 import random
 import discord
 import numpy as np
@@ -18,7 +19,7 @@ BASE_TEAM_TEXT = """
 async def send_teams(ctx: Context, teams: list) -> discord.Message:
     team_text = ""
     for i, team in enumerate(teams, start=1):
-        team_text += "チーム{0}: {1}\n".format(i, ' '.join(mem.mention for mem in team))
+        team_text += "チーム{0}: {1}\n\n".format(i, ' '.join(mem.mention for mem in team))
 
     text = BASE_TEAM_TEXT.format(len(teams), team_text)
     return await ctx.send(text, allowed_mentions=discord.AllowedMentions.none())
@@ -29,15 +30,24 @@ class TeamCog(Cog):
         self.bot = bot
 
     @group(invoke_without_command=True)
+    @guild_only()
     async def team(self, ctx: Context, num: int, *, _members: str) -> None:
-        """チーム分けを行うコマンドです。指定した個数にユーザーを分割します。"""
-        members = list(ctx.message.mentions)
+        """チーム分けを行うコマンドです。指定したチーム数にユーザーを分割します。everyoneと入力すると全員をチーム分けします。"""
+        members = list(ctx.guild.members) if _members == "everyone" else list(ctx.message.mentions)
         if num > len(members):
             await ctx.send("チームの数はメンバーの人数より多くすることはできません。", reference=ctx.message)
             return
 
         random.shuffle(members)
         teams = np.array_split(members, num)
+        await send_teams(ctx, teams)
+
+    @team.command(name="chunked")
+    async def by_member_count(self, ctx: Context, num: int, *, _members: str) -> None:
+        """チームの人数を指定して分割します。everyoneと入力すると全員をチーム分けします。"""
+        members = list(ctx.guild.members) if _members == "everyone" else list(ctx.message.mentions)
+        random.shuffle(members)
+        teams = list(chunked(members, num))
         await send_teams(ctx, teams)
 
 
