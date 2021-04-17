a33cebd19ba3e8f738193237e20ab8ad6993adfa
Tomoya Ishii <shi2gx@gmail.com>
2021-04-17 21:09:47 +0900

    [Fix] ld path

--- heroku_entrypoint.sh
+++ heroku_entrypoint.sh
@@ -4,14 +4,14 @@ DATABASE_URL="${DATABASE_URL//postgres:\/\//postgresql+asyncpg:\/\/}"
 
 export DATABASE_URL="$DATABASE_URL"
 
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/lib"
+export LD_LIBRARY_PATH="/usr/local/lib"
 
 
 echo "================================"
 echo "Database URL: $DATABASE_URL"
 echo "LD Library path: $LD_LIBRARY_PATH"
 echo "Directory: $(pwd)"
-ls
+echo "Libraries: $(ls /usr/local/lib)"
 echo "================================"
 
 bash run.sh
